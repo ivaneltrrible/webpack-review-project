99193de8695892f64ed9a288c4c0306dd2619f09
ivaneltrrible <lucio_21@live.com.mx>
2022-06-30 17:52:16 -0500

    curso webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        //Rules for module extends with .mjs or .js files 
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+  
+} 
